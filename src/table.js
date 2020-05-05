d140b44a264f7355a51e2f0ffe797fe4d6d4ec10
Mario90 <mgpinan1990@gmail.com>
2020-05-05 20:03:24 +0100

    Hability to add items to a table

--- src/table.js
+++ src/table.js
@@ -2,6 +2,10 @@ class Table {
   constructor() {
     this.items = [];
   }
+
+  addItem(item) {
+    this.items.push(item);
+  }
 }
 
 module.exports = Table;
--- test/table.test.js
+++ test/table.test.js
@@ -9,4 +9,15 @@ describe("Table", () => {
   test("has items", () => {
     expect(table.items).toEqual([]);
   });
+
+  test("is able to add items to the list of items", () => {
+    let item = {
+      Coke: 2,
+      "White wine": 4,
+      bolognese: 10,
+      "Sirloing steak": 18,
+    };
+    table.addItem(item);
+    expect(table.items.length).toEqual(1);
+  });
 });
