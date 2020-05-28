019437ccb07b36ef3e7ce5c599a82e9e7fe2498c
Mario90 <mgpinan1990@gmail.com>
2020-05-28 21:25:27 +0100

    Added items to table

--- pos/src/App.js
+++ pos/src/App.js
@@ -28,9 +28,33 @@ class App extends Component {
       { id: 2, number: 3, items: [] },
       { id: 3, number: 4, items: [] },
       { id: 4, number: 5, items: [] },
-      { id: 5, number: 6, items: [] },
+      {
+        id: 5,
+        number: 6,
+        items: [
+          { id: 0, name: "Sirloin", price: 20, amount: 1 },
+          {
+            id: 1,
+            name: "Coke",
+            amount: 3,
+            price: 20,
+          },
+        ],
+      },
       { id: 6, number: 7, items: [] },
-      { id: 7, number: 8, items: [] },
+      {
+        id: 7,
+        number: 8,
+        items: [
+          { id: 0, name: "Sirloin", price: 20, amount: 1 },
+          {
+            id: 1,
+            name: "Coke",
+            amount: 3,
+            price: 20,
+          },
+        ],
+      },
     ],
 
     listItems: [
