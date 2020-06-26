88b84c322af28513aa3dfedb62576b0661b8be74
Mario90 <mgpinan1990@gmail.com>
2020-06-26 20:22:54 +0100

    changed teste

--- restaurant/spec/controllers/table_controller_test.rb
+++ restaurant/spec/controllers/table_controller_test.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
         RSpec.describe Api::V1::TablesController, type: :controller do
             describe 'GET /new ' do
                 it 'responds with 200' do
-                    get :create
+                    get :tables
                     expect(response).to have_http_status(200)
                 end
             end
