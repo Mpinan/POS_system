89620a8d2c10127d9e672ba94c2eaf3ae8f3f82b
Mario90 <mgpinan1990@gmail.com>
2020-07-05 13:19:43 +0100

    Added form

--- restaurant/app/javascript/pos/src/Components/User/signup.js
+++ restaurant/app/javascript/pos/src/Components/User/signup.js
@@ -1,5 +1,13 @@
 import React, { Component } from "react";
-import { Container, Col, Form, FormGroup, Label, Input } from "reactstrap";
+import {
+  Container,
+  Col,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Button,
+} from "reactstrap";
 
 class Signup extends Component {
   constructor(props) {
@@ -8,19 +16,44 @@ class Signup extends Component {
       email: "",
       password: "",
       password_confirmation: "",
+      pin: "",
       errors: "",
     };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log("from submbitted");
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
   render() {
+    const { email, password, password_confirmation, pin, errors } = this.state;
     return (
       <Container>
-        <Form>
+        <Form onSubmit={this.handleSubmit}>
           <FormGroup row>
             <Label for="exampleEmail" sm={2} size="lg">
               Email
             </Label>
             <Col sm={10}>
-              <Input type="email" name="email" placeholder="lg" bsSize="lg" />
+              <Input
+                type="email"
+                name="email"
+                placeholder="lg"
+                bsSize="lg"
+                onChange={this.handleChange}
+                required
+                value={email}
+              />
             </Col>
           </FormGroup>
           <FormGroup row>
@@ -28,7 +61,14 @@ class Signup extends Component {
               Password
             </Label>
             <Col sm={10}>
-              <Input type="password" name="password" placeholder="password" />
+              <Input
+                type="password"
+                name="password"
+                placeholder="password"
+                value={password}
+                onChange={this.handleChange}
+                required
+              />
             </Col>
           </FormGroup>
           <FormGroup row>
@@ -36,7 +76,14 @@ class Signup extends Component {
               Confirm password
             </Label>
             <Col sm={10}>
-              <Input type="password" name="password" placeholder="password" />
+              <Input
+                type="password"
+                name="password_confirmation"
+                placeholder="Confirm password"
+                value={password_confirmation}
+                onChange={this.handleChange}
+                required
+              />
             </Col>
           </FormGroup>
           <FormGroup row>
@@ -46,11 +93,15 @@ class Signup extends Component {
             <Col sm={10}>
               <Input
                 type="password"
-                name="password"
+                name="pin"
                 placeholder="example 123456789"
+                value={pin}
+                onChange={this.handleChange}
+                required
               />
             </Col>
           </FormGroup>
+          <Button type="submit">Submit</Button>
         </Form>
       </Container>
     );
